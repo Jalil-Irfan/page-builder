81b9d404bfb4dd5498bf3d5844f41a5f8c8adf0b
Jalil-Irfan <jalilirfan@gmail.com>
2021-02-04 00:40:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ContentEditable from "react-contenteditable";
 
-function App() {
+class MyComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      html: `<p>Hello <b>World</b> !</p><p>Paragraph 2</p>`,
+      editable: true
+    };
+  }
+  handleChange = evt => {
+    this.setState({ html: evt.target.value });
+  };
+
+  toggleEditable = () => {
+    this.setState({ editable: !this.state.editable });
+  };
+
+  render = () => {
+    return (
+      <div>
+        <h3>editable contents</h3>
+        <ContentEditable
+          className="editable"
+          tagName="pre"
+          html={this.state.html} // innerHTML of the editable div
+          disabled={!this.state.editable} // use true to disable edition
+          onChange={this.handleChange} // handle innerHTML change
+          onBlur={this.sanitize}
+        />
+        <h3>source</h3>
+        <textarea
+          className="editable"
+          value={this.state.html}
+          onChange={this.handleChange}
+          onBlur={this.sanitize}
+        />
+        <h3>actions</h3>
+        <EditButton cmd="italic" />
+        <EditButton cmd="bold" />
+        <EditButton cmd="formatBlock" arg="h1" name="heading" />
+        <EditButton
+          cmd="createLink"
+          arg="https://github.com/lovasoa/react-contenteditable"
+          name="hyperlink"
+        />
+        <button onClick={this.toggleEditable}>
+          Make {this.state.editable ? "readonly" : "editable"}
+        </button>
+      </div>
+    );
+  };
+}
+
+function EditButton(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      key={props.cmd}
+      onMouseDown={evt => {
+        evt.preventDefault(); // Avoids loosing focus from the editable area
+        document.execCommand(props.cmd, false, props.arg); // Send the command to the browser
+      }}
+    >
+      {props.name || props.cmd}
+    </button>
   );
 }
 
-export default App;
+
+export default MyComponent;
